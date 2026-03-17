f9616fabb82ca3cd9aa60dc3b62a1a416ea11a05
Harshwardhan sharma <harshwardhans279sharma@gmail.com>
2026-03-17 17:22:02 +0530

    Update firebase.js

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -4,15 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAR1eQegjPcG9Yk23EP4G9wWneqChxnNPU",
-  authDomain: "vyapar-ai-31ef7.firebaseapp.com",
-  projectId: "vyapar-ai-31ef7",
-  storageBucket: "vyapar-ai-31ef7.firebasestorage.app",
-  messagingSenderId: "38725306636",
-  appId: "1:38725306636:web:e64b447af627798838e129",
-  measurementId: "G-F9618D9V5X"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
